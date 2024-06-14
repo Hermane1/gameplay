cdfcb4c8f218e8f9b9de4d033daff7e0159ac09f
Hermane1 <essence.herman404@gmail.com>
2024-06-14 13:51:59 -0400

    added a new code to the old one

--- rockpapersicssors.js
+++ rockpapersicssors.js
@@ -1,12 +1,9 @@
-// Define the hands array
 const hands = ['rock', 'paper', 'scissors'];
 
-// Define the getHand function
 function getHand() {
     return hands[parseInt(Math.random() * 10) % 3];
 }
 
-// Define two player objects
 const player1 = {
     name: 'Player 1',
     getHand: getHand
@@ -17,7 +14,16 @@ const player2 = {
     getHand: getHand
 };
 
-// Define the playRound function
+const player3 = {
+    name: 'Player 3',
+    getHand: getHand
+};
+
+const player4 = {
+    name: 'Player 4',
+    getHand: getHand
+};
+
 function playRound(player1, player2) {
     const hand1 = player1.getHand();
     const hand2 = player2.getHand();
@@ -43,9 +49,53 @@ function playRound(player1, player2) {
     return winner;
 }
 
-// Export the playRound function and player objects for testing purposes
+function playGame(player1, player2, playUntil) {
+    let player1Wins = 0;
+    let player2Wins = 0;
+
+    while (player1Wins < playUntil && player2Wins < playUntil) {
+        const winner = playRound(player1, player2);
+
+        if (winner === player1) {
+            player1Wins++;
+        } else if (winner === player2) {
+            player2Wins++;
+        }
+
+        console.log(`${player1.name} has ${player1Wins} wins`);
+        console.log(`${player2.name} has ${player2Wins} wins`);
+    }
+
+    const finalWinner = player1Wins === playUntil ? player1 : player2;
+    console.log(`${finalWinner.name} wins the game!`);
+    return finalWinner;
+}
+
+function playTournament(player1, player2, player3, player4, playUntil) {
+    console.log("Round 1:");
+    const winner1 = playGame(player1, player2, playUntil);
+
+    console.log("Round 2:");
+    const winner2 = playGame(player3, player4, playUntil);
+
+    console.log("Final Round:");
+    const champion = playGame(winner1, winner2, playUntil);
+
+    console.log(`${champion.name} is the world champion!`);
+}
+
+
+playGame(player1, player2, 3);
+
+
+playTournament(player1, player2, player3, player4, 3);
+
 module.exports = {
     playRound,
+    playGame,
+    playTournament,
     player1,
-    player2
+    player2,
+    player3,
+    player4
 };
